45dfadd8bd67f6e53ad3145f2a64bc7ea0916ef4
Gokul312 <gokulakrishnanr2000@gmail.com>
2025-03-20 10:37:05 +0530

    Added Ping Pong

--- app/server.rb
+++ app/server.rb
@@ -11,7 +11,28 @@ class YourRedisServer
 
     # Uncomment this block to pass the first stage
     server = TCPServer.new(@port)
-    client = server.accept
+    loop do
+      Thread.start(server.accept) do |client|
+        raw_request = []
+        raw_request << client.gets
+        num_args = raw_request.first.strip[1..-1].to_i
+        args = []
+        num_args.times do
+          size = client.gets.strip[1..-1].to_i  # Read the size of each argument
+          arg = client.read(size)  # Read the argument itself
+          args << arg
+          client.gets  # Consume the \r\n after the argument
+        end
+        command = args.shift
+        if command.downcase == "ping"
+          response = "+PONG\r\n"
+        else
+          response = "+ERR-command not found\r\n"
+        end
+        client.puts response
+        client.close
+      end
+    end
   end
 end
 
